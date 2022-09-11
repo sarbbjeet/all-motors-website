477bf7ffd9effda6fa03e8d3647196176ccbbe1b
Sarbjit <devcodetest1@gmail.com>
2022-09-11 13:11:37 +0100

    manually cors implemented

--- next.config.js
+++ next.config.js
@@ -12,6 +12,31 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-CSRF-Token ,X-Requested-With, Content, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 module.exports = nextConfig;
 // module.exports = withPlugins([[withImages]], nextConfig);
